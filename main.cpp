a88ac05b2a33d8ebc8b3f45f14708d88f0c98880
Eric R <fake@email.com>
2016-07-19 16:27:39 -0500

    working with vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <vector>
 #include "Bar.h"
+#include "smaCross.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
 
-    Bar bar;
-    //cout << source;
+    Bar bar1;
+    bar1.close=1;
+    Bar bar2;
+    bar2.close=2;
+
+    vector<Bar> bars = {bar1, bar2};
+
+    strat::smaCross(bars, 0);
+
 
     return 0;
 }
--- smaCross.h
+++ smaCross.h
@@ -7,31 +7,42 @@
 
 #include <vector>
 #include "Bar.h"        //include the Bar struct. Bad practice to include in a header file?
-#include "sma.h"        //todo: make SMA function
+//#include "sma.h"        //todo: make SMA function
 
 #include <iostream>
 #include <algorithm>
+namespace strat {
 
-enum signal_t {SELL = -1, BUY = 1, HOLD = 0, ERR = -2};
-enum ohlc_t {open=Bar.open, high=Bar.high, low=Bar.low, close=Bar.close};   //close most often used
+    enum signal_t {
+        SELL = -1, BUY = 1, HOLD = 0, ERR = -2
+    };
+    enum ohlc_t {
+        open, high, low, close
+    };
 
-//  Function smaCross
-//  series: the entire vector of Bars, each bar containing 1 unit of price data
-//  source: which price point to base calculations on: open, high, low, or close
-//  length: how far back the SMA looks when calculating average
+    //  Function smaCross
+    //  series: the entire vector of Bars, each bar containing 1 unit of price data
+    //  length: how far back the SMA looks when calculating average
+    //  returns:  BUY when source crosses above sma, SELL when below, else hold
 
+    signal_t smaCross(const std::vector<Bar>& series, int length) {
+        // ... if the SMA length is 50 bars, we can't begin calculation until the 50th bar
+        // ... so some code goes here to start the for loop at some length
+        // ... if 2+ parameters are given, the longest length will be the one to consider
+        int longestLength = length;
 
-signal_t smaCross(const std::vector<Bar>& series, ohlc_t source, int length){
-    // ... if the SMA length is 50 bars, we can't begin calculation until the 50th bar
-    // ... so some code goes here to start the for loop at some length
-    // ... if two parameters are given, the longest length will be the one to consider
-    int longestLength = length;
+        //using std::begin; using std::end; using std::next;
+        //std::for_each(next(begin(series), longestLength), end(series), [](auto&& i) {
+        //     });
 
-    using std::begin; using std::end; using std::next;
-    std::for_each(next(begin(series), longestLength), end(series), [](auto&& i) {
-         });       //use at() for debugging, but operator [] is faster
+        for(std::vector<Bar>::const_iterator i = (series.begin()+longestLength); i != series.end(); ++i){
+            std::cout << i->close;
+        }
 
-    return ERR;
+        return ERR;
+    }
 }
+
+
 
 #endif //TRADERCS4398_SMACROSS_H
