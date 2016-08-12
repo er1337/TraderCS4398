1677f8ff03831290d9d23b78745b10ee93e30425
er1337 <shmoofolamburger@gmail.com>
2016-08-12 02:55:39 -0500

    take 2
    try again

--- parameterOptimize.h
+++ parameterOptimize.h
@@ -2,37 +2,45 @@
 
 #ifndef TRADERCS4398_PARAMETEROPTIMIZE_H
 #define TRADERCS4398_PARAMETEROPTIMIZE_H
+#include <iomanip>
 
 //Going to loop through all possible outcomes and show best results
 void parameterOptimize(Account& a, const vector<Bar> ps, const float fee=0){
     vector<signal_t> signalSeries;
-    cout << a.getInitBalance() << endl;
-    cout << "Sma Price Cross \n";
+    float initBalance = a.getInitBalance();
+    cout << initBalance << endl;
+    cout << "Double EMA Cross \n";
     cout << "i\tmoney\ttrades\n";
     int end = ps.size()/2;
-    int bestI;
+    int bestI, bestK;
     int bestNumTrades = 0;
     float bestProfitPerTrades = 0,
           bestProft = 0;
-    for(int i=2; i<ps.size()/2; ++i){
+    int upperBound = ps.size()/2;
+    for(int i=2; i<upperBound; ++i){
+      int kUpperBound = 0.9*i;
+      //for(int k=2; k<i-10; ++k){
+      for(int k=2; k<i-10; k+=20){
         a.setBalance(a.getInitBalance());
-        signalSeries = strat::smaCross(ps, i);
+        signalSeries = strat::doubleEmaCross(ps, k, i);
         StrategyRunner sr(a, ps, signalSeries, fee);
         sr.runStrategy(false);
-        if(a.getBalance() > a.getInitBalance()) {
-            cout << i << "\t" << a.getBalance() << "\t" << sr.getNumTrades() << endl;
+        if(a.getBalance() > initBalance) {
+            cout << i << "," << k << setw(10) << a.getBalance() << setw(10) << sr.getNumTrades() << endl;
             float profitpertrades = a.getBalance() / sr.getNumTrades();
             if(profitpertrades > bestProfitPerTrades){
                 bestProfitPerTrades = profitpertrades;
                 bestI = i;
+                bestK = k;
                 bestProft = a.getBalance();
                 bestNumTrades = sr.getNumTrades();
             }
         }
-        if(i%(end/10)==0) cout << (int)(((float)i/(float)end)*100) << "%" << endl;
+      }
+      if(i%(end/10)==0) cout << (int)(((float)i/(float)end)*100) << "%" << endl;
     }
     cout << "done" << endl;
-    cout << "Best value: "<<bestI << " gave profit $" << bestProft << " with "<< bestNumTrades << " trades.\n";
+    cout << "Best i: "<<bestI <<" k: "<<bestK << " gave profit $" << bestProft << " with "<< bestNumTrades << " trades.\n";
 
 }
 
